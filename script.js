9d01ee15bf25fa67bf7c1f914659ee39224d990a
resty-aldave <restyaldave84@gmail.com>
2026-01-31 12:55:26 +0800

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const moreIcon = document.getElementById('more-icon');
 const navText = document.getElementById('nav-text');
 const menuOverlay = document.getElementById('menu-overlay');
 const navLinks = document.querySelectorAll('#nav-text a');
+const quotes = document.querySelectorAll('.quotes');
+const heroImg = document.querySelectorAll('.hero-img');
 
+// navbar or menu
 const toggleMenu = () => {
     navText.classList.toggle('is-active');
     menuOverlay.classList.toggle('is-active');
@@ -17,3 +20,27 @@ navLinks.forEach(link => {
         menuOverlay.classList.remove('is-active');
     });
 });
+
+// quotes
+let currentQuoteIndex = 0;
+
+function showNextQuote() {
+
+    quotes[currentQuoteIndex].classList.remove('active');
+    currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
+    quotes[currentQuoteIndex].classList.add('active');
+}
+
+setInterval(showNextQuote, 5000);
+
+// hero-img
+let currentHeroImgIndex = 0;
+
+function showNextHeroImg() {
+
+    heroImg[currentHeroImgIndex].classList.remove('actives');
+    currentHeroImgIndex = (currentHeroImgIndex + 1) % heroImg.length;
+    heroImg[currentHeroImgIndex].classList.add('actives');
+}
+
+setInterval(showNextHeroImg, 8000);
